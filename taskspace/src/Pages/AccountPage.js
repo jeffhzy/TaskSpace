cbfa25b52ffd5d265ab36297e63a8cf8671f847e
Jingting203 <e0772715@u.nus.edu>
2022-07-06 21:57:31 +0800

    slight adjustments

--- taskspace/src/Pages/AccountPage.js
+++ taskspace/src/Pages/AccountPage.js
@@ -1,21 +1,14 @@
 import "./AccountPage.css";
-import Sidebar from "../Components/Sidebar/Sidebar";
-import Header from "../Components/Header/Header";
 import ProfilePicUpload from "../Components/Account/ProfilePicUpload/ProfilePicUpload";
 
 
 const AccountPage = () => {
     return (
-        <>
-            <Header />
             <div className="pageOrientation">
-                <Sidebar className="header-sidebar" values={{}} />
                 <div className="mainPart">
                     <ProfilePicUpload />
                 </div>
             </div>
-        </>
-
     );
 };
 
