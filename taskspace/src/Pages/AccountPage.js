70c220e6b21d8ee51fbe1397aa423e928f4d5d5e
Jeff Hong <jeffhzyy@gmail.com>
2022-07-06 22:25:05 +0800

    added routing for account

---
+++ taskspace/src/Pages/AccountPage.js
@@ -0,0 +1,11 @@
+import ProfilePicUpload from "../Components/Account/ProfilePicUpload/ProfilePicUpload";
+
+const AccountPage = () => {
+  return (
+    <div>
+      <ProfilePicUpload />
+    </div>
+  );
+};
+
+export default AccountPage;
